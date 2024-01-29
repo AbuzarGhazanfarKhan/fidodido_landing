abcf2901aec5262d81bb5da9b7738c3daa1a0dc7
ahmedraza90 <34ahmed.raza@gmail.com>
2024-01-29 07:40:19 +0500

    walletChecker Completed

--- src/pages/WalletChecker/index.js
+++ src/pages/WalletChecker/index.js
@@ -3,34 +3,36 @@ import './walletChecker.css';
 import React, { useState } from 'react';
 import { FaArrowRightLong } from "react-icons/fa6";
 import okFido from '../../assets/Links/10.webp'
+import NoFido from '../../assets/Links/37.webp'
+import axios from "axios";
 
 function WalletChecker() {
-    const [searchTerm, setSearchTerm] = useState('');
     const [showModal, setShowsModal] = useState(false);
+    const [address, setAddress] = useState("")
+    const [verify, setVerify] = useState("loading")
 
     const handleInputChange = event => {
-        setSearchTerm(event.target.value);
+        setAddress(event.target.value);
     };
 
-    // const fetchWalletStatus = async () => {
-    //     try {
-    //       const response = await axios.get(`https://qr-code-api.oasisx.world/check-wallet/${address}`);
-    //       if (response.data.status === "Success") {
-    //         setProof(response.data.data.proof)
-    //         return true
-    //       } else {
-    //         return false
-    //       }
-    //     } catch (error) {
-    //       console.error(`Error fetching wallet status: ${error}`);
-    //     }
-    // };
+    const fetchWalletStatus = async () => {
+        setShowsModal(true)
+        try {
+            const response = await axios.get(`https://qr-code-api.oasisx.world/check-wallet/${address}`);
+            if (response.data.status === "Success") {
+                setVerify("true")
+            } else {
+                setVerify("false")
+            }
+        } catch (error) {
+            console.error(`Error fetching wallet status: ${error}`);
+        }
+    };
 
     const importAllImages = (r) => r.keys().map(r);
     const images = importAllImages(
         require.context("../../assets/Links", false, /\.(png|jpe?g|svg|gif|webp)$/)
     );
-    console.log(images);
     return (
         <div style={{ background: "black" }}>
             <div className="grid-container">
@@ -47,30 +49,55 @@ function WalletChecker() {
                 <div className="search-bar">
                     <h1 style={{ color: "white" }}>Let's See If You Can Mint</h1>
                     <div className="input-wrapper">
-                    <center>    <input className="searchbar" type="text" value={searchTerm} onChange={handleInputChange} placeholder='Please Enter Your Wallet Address' />
-                        <span onClick={()=>setShowsModal(true)} className='submitButton'>
-                            <FaArrowRightLong color='#009016' size={40} />
-                        </span>
+                        <center>    <input className="searchbar" type="text" value={address} onChange={handleInputChange} placeholder='Please Enter Your Wallet Address' />
+                            <span onClick={fetchWalletStatus} className='submitButton'>
+                                <FaArrowRightLong color='#009016' size={40} />
+                            </span>
                         </center>
                     </div>
                 </div>
             </center>
             {showModal ? (
-        <div className="modal">
-          <div className="modal-content">
-          <span className="close" onClick={()=>setShowsModal(false)}>X</span>
-          <div className='modalSection'>
-            <div className="left-section">
-              <img src={okFido} alt="" />
-            </div>
-            <div className="right-section">
-              <p>YOUR JOURNEY STARTS NOW, 
-YOU’VE BEEN WHITELISTED</p>
-            </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={() => setShowsModal(false)}>X</span>
+                        <div className='modalSection'>
+                            {
+                                verify === "true" ?
+                                    <>
+                                        <div className="left-section">
+                                            <img src={okFido} alt="" />
+                                        </div>
+                                        <div className="right-section">
+                                            <p>YOUR JOURNEY STARTS NOW,
+                                                YOU’VE BEEN WHITELISTED</p>
+                                        </div>
+                                    </> : verify === "false" ?
+                                    <>
+                                        <div className="left-section">
+                                            <img src={NoFido} alt="" />
+                                        </div>
+                                        <div className="right-section">
+                                            <p> NOT YET
+                                                YOU’VE BEEN WHITELISTED YET.
+                                                TAKE YOUR LAST CHANCE WL ON
+                                                THE PIN TWEET</p>
+                                        </div>
+                                    </> : 
+                                    <>
+                                    <div className="left-section">
+                                        <img src={okFido} alt="" />
+                                    </div>
+                                    <div className="right-section">
+                                        <p>....</p>
+                                    </div>
+                                </> 
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            ) : null}
         </div>
     )
 }
